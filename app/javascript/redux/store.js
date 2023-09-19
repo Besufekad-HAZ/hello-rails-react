0c8c99d9468c0694c391743920eda4afbb78a747
Besufekad-HAZ <besurye@gmail.com>
2023-09-20 00:07:45 +0300

    Add neccescary built in library and dependency files

--- app/javascript/redux/store.js
+++
@@ -1,4 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import greetingsReducer from './greetingsSlice';
-
-export default configureStore({ reducer: greetingsReducer });
--- app/models/greeting.rb
+++
@@ -1,4 +0,0 @@
-class Greeting < ApplicationRecord
-  # Add a message attribute
-  attribute :content, :string
-end
